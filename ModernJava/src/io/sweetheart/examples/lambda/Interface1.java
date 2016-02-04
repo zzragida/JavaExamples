aa36a5fa01d1e507462b67bd3e59074c02783029
JungSu Kim <zzragida@gmail.com>
2016-02-04 09:11:23 +0900

    add modern java examples

---
+++ ModernJava/src/io/sweetheart/examples/lambda/Interface1.java
@@ -0,0 +1,28 @@
+package io.sweetheart.examples.lambda;
+
+public class Interface1 {
+    interface Formula {
+        double calculate(int a);
+
+        default double sqrt(int a) {
+            return Math.sqrt(positive(a));
+        }
+
+        static int positive(int a) {
+            return a > 0 ? a : 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Formula formula = new Formula() {
+            @Override
+            public double calculate(int a) {
+                return sqrt(a * 100);
+            }
+        };
+
+        System.out.println(formula.calculate(100));
+        System.out.println(formula.sqrt(-23));
+        System.out.println(Formula.positive(-4));
+    }
+}
